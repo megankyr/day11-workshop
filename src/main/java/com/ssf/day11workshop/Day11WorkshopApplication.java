ca19c7e1e4dcc0dcbbf35fbeb3a71448118aaab9
Megan <megan.koh96@gmail.com>
2023-11-22 23:18:18 +0800

    Initial Commit

--- src/main/java/com/ssf/day11workshop/Day11WorkshopApplication.java
+++ src/main/java/com/ssf/day11workshop/Day11WorkshopApplication.java
@@ -1,5 +1,9 @@
 package com.ssf.day11workshop;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day11WorkshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day11WorkshopApplication.class, args);
+		SpringApplication app = new SpringApplication(Day11WorkshopApplication.class);
+
+		String port = portFromCommandLine(args);
+
+		if (port == null) {
+			port = portFromEnvironment();
+		}
+
+		if (port == null) {
+			port = "3000";
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		System.out.println("Application started on port " + port);
+		app.run(args);
+	}
+
+	private static String portFromCommandLine(String[] args) {
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+		if (cliOpts.containsOption("port")) {
+			return cliOpts.getOptionValues("port").get(0);
+		}
+		return null;
+	}
+
+	private static String portFromEnvironment() {
+		String port = System.getenv("PORT");
+		if (port != null) {
+			return port;
+		}
+		return null;
 	}
 
 }
