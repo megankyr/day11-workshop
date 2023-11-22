b914679cb146961283d24b93de68812a48408430
Megan <megan.koh96@gmail.com>
2023-11-22 15:02:50 +0800

    Initial Commit

---
+++ src/main/java/com/ssf/day11workshop/Day11WorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.ssf.day11workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssf/day11workshop/Day11WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssf.day11workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
